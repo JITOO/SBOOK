e2304bde7999b71f333289db01efae79d18b9e99
JITOO <leejisu1584@gmail.com>
2015-07-03 20:34:55 +0900

    오픈한글 api 이용해서 기본형 판별 후 감정을 추출해내는 소스

--- OpinonParsing.py
+++ OpinonParsing.py
@@ -4,19 +4,33 @@
 # 오픈한글 감성사전 긍정,부정 JSON 파싱
 import requests
 import json
+import urllib
+import BeautifulSoup
 
 word_input = raw_input("word input : ")
+url = 'http://api.openhangul.com/basic?q=' + word_input
 
 
+r = requests.get(url)
 
-sentiment_url = 'http://api.openhangul.com/dic?api_key=20141107174448&q='+word_input
+data = urllib.urlopen(url)
+soup = BeautifulSoup.BeautifulSoup(data)
+soup_split = soup.prettify()
+div = soup_split.split(",")
+soup_split2 = div[1]
+div2 = soup_split2.split(":")
+soup_split3 = div2[1]
+div3 = soup_split3.split('"')
+basicword = div3[1]
+
+
+sentiment_url = 'http://api.openhangul.com/dic?api_key=20141107174448&q='+basicword
 sentiment_request = requests.get(sentiment_url)
 
 try:
     decoded = json.loads(sentiment_request.text)
 
     # pretty printing of json-formatted string
-    print json.dumps(decoded, sort_keys=True, indent=1  )
 
     print "JSON parsing example: ", decoded['word']
     print "JSON parsing example: ", decoded['type']
